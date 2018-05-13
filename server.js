dcfd2037ddfcc1b49d451415c975eed7b961b8e9
Ayan Majumder <ayon4mclick@gmail.com>
2018-05-14 00:05:13 +0530

    Fixed server configuration

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');4
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, '/dist')));
+
 app.use('/', (req, res) => {
-    res.send(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 
 app.listen(3000);
